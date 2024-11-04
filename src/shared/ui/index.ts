4d2ee53cf0bf5ec526ea3a806c8133010fd3de3e
YHJ96 <yhj878812@naver.com>
2024-11-04 17:30:30 +0900

    feat: bookmark-modal component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,46 @@
+import { Button } from './Button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './Card';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from './Dialog';
+import { Input } from './Input';
+import { Label } from './Label';
 import { Tag } from './Tag';
 
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle };
-export { Tag };
+export {
+  /* Card */
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  /* Dialog */
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+  /* Button */
+  Button,
+  /* Tag */
+  Tag,
+  /* Label */
+  Label,
+  /* Input */
+  Input,
+};
