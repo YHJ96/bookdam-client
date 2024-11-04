3a9cc2bdb3f23aae0f73c7323568e7d8a212cdda
YHJ96 <yhj878812@naver.com>
2024-11-04 15:38:47 +0900

    feat: tag component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,5 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './Card';
+import { Tag } from './Tag';
 
 export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle };
+export { Tag };
