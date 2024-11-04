f7b4c4e918178a9c42b80c3834199f58e81e2423
YHJ96 <yhj878812@naver.com>
2024-11-04 22:02:40 +0900

    feat: theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 
-import { QueryProvider } from '@/providers';
+import { QueryProvider, ThemeProvider } from '@/providers';
 import { font } from '@/shared/libs';
 
 import './globals.css';
@@ -14,11 +14,14 @@ type RooRootLayoutProps = {
   children: React.ReactNode;
 };
 
+/* https://github.com/pacocoursey/next-themes/blob/bf0c5a45eaf6fb2b336a6b93840e4ec572bc08c8/next-themes/README.md?plain=1#L95C1-L96C1 */
 function RootLayout({ children }: RooRootLayoutProps) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body className={font.className}>
-        <QueryProvider>{children}</QueryProvider>
+        <ThemeProvider>
+          <QueryProvider>{children}</QueryProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
