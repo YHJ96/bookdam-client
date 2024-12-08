ab5ce372e74a0c373fb01074b9fb1fbf371cd500
YHJ96 <yhj878812@naver.com>
2024-12-08 21:52:23 +0900

    feat: login

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_KAKAO_LOGIN_URL: string;
+    NEXT_PUBLIC_GOOGLE_LOGIN_URL: string;
+  }
+}
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,6 +1,7 @@
 import Home from './Home';
 import Layout from './Layout';
+import Login from './Login';
 import Remove from './Remove';
 import Tour from './Tour';
 
-export { Home, Layout, Remove, Tour };
+export { Home, Layout, Remove, Tour, Login };
