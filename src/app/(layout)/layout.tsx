2ddc9fc046beb5d6e443e8f5f167852fa3eb20f5
YHJ96 <yhj878812@naver.com>
2024-12-12 21:24:07 +0900

    feat: server utils

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from 'next';
-import { cookies } from 'next/headers';
+import { unstable_cache } from 'next/cache';
 
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { getBookmark } from '@/entities/bookmark/api';
 import { QueryProvider, ThemeProvider } from '@/providers';
 import { font } from '@/shared/libs';
 import { DialogProvider } from '@/shared/ui';
-import { decrypt, jwtDecode } from '@/shared/utils';
+import { cookieWrapper, getSession } from '@/shared/utils/server';
 import { Layout } from '@/views';
 
 import './globals.css';
@@ -21,22 +21,22 @@ type RooRootLayoutProps = {
   children: React.ReactNode;
 };
 
-async function getSession() {
-  const cookie = await cookies();
-  const accessToken = cookie.get('access')?.value ?? '';
-  const jwt = jwtDecode(accessToken);
-  if (!(jwt && 'ec' in jwt)) return null;
-  return decrypt(jwt.ec);
-}
-
 /* https://github.com/pacocoursey/next-themes/blob/bf0c5a45eaf6fb2b336a6b93840e4ec572bc08c8/next-themes/README.md?plain=1#L95C1-L96C1 */
 async function RootLayout({ children }: RooRootLayoutProps) {
   const queryClient = new QueryClient();
   const session = await getSession();
 
-  if (session) await queryClient.prefetchQuery({ queryKey: ['user'], queryFn: getSession });
+  if (session) {
+    await queryClient.prefetchQuery({ queryKey: ['user'], queryFn: getSession });
 
-  const dehydratedState = dehydrate(queryClient);
+    await queryClient.prefetchQuery({
+      queryKey: ['bookmark'],
+      queryFn: unstable_cache(cookieWrapper(session.accessToken, session.refreshToken, getBookmark), [session.id], {
+        revalidate: 3600,
+        tags: ['bookmark'],
+      }),
+    });
+  }
 
   return (
     <html lang="ko" suppressHydrationWarning>
@@ -44,11 +44,10 @@ async function RootLayout({ children }: RooRootLayoutProps) {
         <ThemeProvider>
           <QueryProvider>
             <DialogProvider>
-              <HydrationBoundary state={dehydratedState}>
+              <HydrationBoundary state={dehydrate(queryClient)}>
                 <Layout>{children}</Layout>
               </HydrationBoundary>
             </DialogProvider>
-            <ReactQueryDevtools />
           </QueryProvider>
         </ThemeProvider>
       </body>
