1d987845650dcf95f52b2ec79e085ae56fe537ec
YHJ96 <yhj878812@naver.com>
2024-12-31 03:35:54 +0900

    feat: colors and typography storybook

--- .storybook/utils/GlobalDecorator.tsx
+++ .storybook/utils/GlobalDecorator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import type { Decorator } from '@storybook/react';
 import { themes } from '@storybook/theming';
