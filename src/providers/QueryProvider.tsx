2c73667c44c78e24560a09cfa37bd998e33ea41e
YHJ96 <yhj878812@naver.com>
2024-12-30 15:16:20 +0900

    feat: mutation query error toast

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -6,6 +6,7 @@ import { MutationCache, QueryCache, QueryClient, QueryClientProvider, Register }
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AxiosError } from 'axios';
 
+import { useToast } from '@/shared/hooks';
 import { nextApi } from '@/shared/libs';
 
 type QueryProviderProps = {
@@ -13,6 +14,8 @@ type QueryProviderProps = {
 };
 
 export default function QueryProvider({ children }: QueryProviderProps) {
+  const { toast } = useToast();
+
   const [client] = useState(
     () =>
       new QueryClient({
@@ -24,10 +27,16 @@ export default function QueryProvider({ children }: QueryProviderProps) {
             if (err.status === 401) {
               await nextApi.delete('/cookie');
               client.setQueryData(['user'], null);
+              return;
             }
+
+            toast({ title: '에러가 발생했습니다.', description: '잠시후에 시도해주세요.', variant: 'destructive' });
           },
         }),
         mutationCache: new MutationCache({
+          onSuccess: () => {
+            toast({ title: '성공하였습니다.', description: '요청이 성공적으로 반영되었습니다.', variant: 'default' });
+          },
           onError: async (err, variables, context, mutation) => {
             if (!(err instanceof AxiosError)) return;
             if (mutation.meta?.isThrowError) return;
@@ -35,7 +44,10 @@ export default function QueryProvider({ children }: QueryProviderProps) {
             if (err.status === 401) {
               await nextApi.delete('/cookie');
               client.setQueryData(['user'], null);
+              return;
             }
+
+            toast({ title: '에러가 발생했습니다.', description: '잠시후에 시도해주세요.', variant: 'destructive' });
           },
         }),
       }),
