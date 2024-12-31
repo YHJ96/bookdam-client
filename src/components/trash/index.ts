efc7b3c19a127193faf91eac9ef6b8d0f1ef504e
YHJ96 <yhj878812@naver.com>
2024-12-31 18:46:12 +0900

    refactor: trash bookmark data flow modify

--- src/components/trash/index.ts
+++ src/components/trash/index.ts
@@ -1,4 +1,4 @@
+import TrashBookmark from './trash-bookmark';
 import TrashBookmarkEmpty from './trash-bookmark-empty';
-import TrashBookmark from './trash-bookmark/TrashBookmark';
 
 export { TrashBookmark, TrashBookmarkEmpty };
