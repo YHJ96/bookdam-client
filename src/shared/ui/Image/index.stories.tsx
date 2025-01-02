4f372b1d94adfb6df6f0f7d1282e8a44cdd4cf02
YHJ96 <yhj878812@naver.com>
2025-01-02 23:03:16 +0900

    feat: image storybook docs

--- src/shared/ui/Image/index.stories.tsx
+++ src/shared/ui/Image/index.stories.tsx
@@ -6,6 +6,12 @@ const meta = {
   title: 'Shared/Image',
   component: Image,
   tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+    docs: {
+      subtitle: 'Image 컴포넌트는 이미지를 표시하는 데 사용됩니다.',
+    },
+  },
   argTypes: {
     src: {
       control: {
@@ -35,6 +41,13 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: '기본적인 이미지 컴포넌트입니다.',
+      },
+    },
+  },
   args: {
     src: 'https://picsum.photos/200/300',
     alt: 'Image',
