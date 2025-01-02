9c8ce07642d9c8f2f6be20817a581aa7029e301e
YHJ96 <yhj878812@naver.com>
2025-01-03 00:43:28 +0900

    feat: bookmark tag filter storybook docs

--- src/components/home/bookmark-tag-filter/index.stories.tsx
+++ src/components/home/bookmark-tag-filter/index.stories.tsx
@@ -6,6 +6,11 @@ import BookmarkTagFilter from './index';
 const meta: Meta<typeof BookmarkTagFilter> = {
   title: 'Component/BookmarkTagFilter',
   component: BookmarkTagFilter,
+  parameters: {
+    docs: {
+      subtitle: 'BookmarkTagFilter 컴포넌트는 북마크 태그를 필터링하는 데 사용됩니다.',
+    },
+  },
   argTypes: {
     tags: {
       control: {
