7731c5420f5ad6d997f0da0c93c5846294fc5e04
YHJ96 <yhj878812@naver.com>
2025-01-03 00:37:38 +0900

    feat: trash bookmark empty storybook docs

--- src/components/trash/trash-bookmark-empty/index.stories.tsx
+++ src/components/trash/trash-bookmark-empty/index.stories.tsx
@@ -5,6 +5,13 @@ import TrashBookmarkEmpty from './index';
 const meta: Meta<typeof TrashBookmarkEmpty> = {
   title: 'Component/TrashBookmarkEmpty',
   component: TrashBookmarkEmpty,
+  parameters: {
+    docs: {
+      description: {
+        story: '휴지통에 들어있는 북마크가 없을 때 표시되는 컴포넌트입니다.',
+      },
+    },
+  },
 } satisfies Meta<typeof TrashBookmarkEmpty>;
 
 export default meta;
