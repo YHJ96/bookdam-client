fbe007e279f468ee760300fb589d995789827319
YHJ96 <yhj878812@naver.com>
2025-01-02 22:57:49 +0900

    feat: badge storybook docs

--- src/shared/ui/Badge/index.stories.tsx
+++ src/shared/ui/Badge/index.stories.tsx
@@ -17,18 +17,43 @@ const meta = {
   tags: ['autodocs'],
   parameters: {
     layout: 'centered',
+    docs: {
+      subtitle: 'Badge 컴포넌트는 뱃지를 표시하는 데 사용됩니다.',
+    },
   },
 } satisfies Meta<typeof Badge>;
 
-export const Default: StoryObj<typeof Badge> = {};
+export const Default: StoryObj<typeof Badge> = {
+  parameters: {
+    docs: {
+      description: {
+        story: '기본적인 뱃지 컴포넌트입니다. 뱃지를 표시합니다.',
+      },
+    },
+  },
+};
 
 export const Selected: StoryObj<typeof Badge> = {
   args: {
     variant: 'selected',
   },
+  parameters: {
+    docs: {
+      description: {
+        story: '선택된 뱃지 컴포넌트입니다. 뱃지를 클릭한 경우 선택된 상태로 변경됩니다.',
+      },
+    },
+  },
 };
 
 export const Delete: StoryObj<typeof Badge> = {
+  parameters: {
+    docs: {
+      description: {
+        story: '삭제 뱃지 컴포넌트입니다. X 아이콘을 클릭하는 경우 뱃지가 삭제됩니다.',
+      },
+    },
+  },
   render: ({ children, ...rest }) => (
     <div className="flex items-center gap-2">
       <Badge {...rest}>
