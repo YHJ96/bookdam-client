f92f82630ca322f7c356cbde7049b49a40fb90e2
YHJ96 <yhj878812@naver.com>
2025-01-02 23:03:46 +0900

    feat: input storybook docs

--- src/shared/ui/Input/index.stories.tsx
+++ src/shared/ui/Input/index.stories.tsx
@@ -18,6 +18,9 @@ const meta = {
   },
   parameters: {
     layout: 'centered',
+    docs: {
+      subtitle: 'Input 컴포넌트는 텍스트 입력을 받는 데 사용됩니다.',
+    },
   },
   render: (args) => <Input {...args} className="w-96" />,
 } satisfies Meta<typeof Input>;
@@ -26,8 +29,23 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: '기본적인 입력 컴포넌트입니다.',
+      },
+    },
+  },
+};
 
 export const Disabled: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: '비활성화된 입력 컴포넌트입니다.',
+      },
+    },
+  },
   args: { disabled: true },
 };
