159626e50e457b6284ad9fe136bb9712b81bf34e
YHJ96 <yhj878812@naver.com>
2025-01-02 23:07:08 +0900

    feat: toast storybook docs

--- src/shared/ui/Toast/index.stories.tsx
+++ src/shared/ui/Toast/index.stories.tsx
@@ -16,6 +16,9 @@ const meta = {
   },
   parameters: {
     layout: 'centered',
+    docs: {
+      subtitle: 'Toast 컴포넌트는 토스트를 표시하는 데 사용됩니다.',
+    },
   },
   decorators: [
     (Story) => (
@@ -51,6 +54,13 @@ type Story = Omit<StoryObj<typeof meta>, 'args'> & {
 };
 
 export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: '기본적인 토스트 컴포넌트입니다.',
+      },
+    },
+  },
   args: {
     title: '메세지 제목',
     description: '메세지 내용',
@@ -58,6 +68,13 @@ export const Default: Story = {
 };
 
 export const Destructive: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: '에러 메세지를 표시하는 토스트 컴포넌트입니다.',
+      },
+    },
+  },
   args: {
     variant: 'destructive',
     title: '메세지 제목',
