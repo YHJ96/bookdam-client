3f7a957334fcdeb798d32733ffdb6552b9489f99
YHJ96 <yhj878812@naver.com>
2025-01-03 20:03:21 +0900

    test: sidebar component test

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -3,3 +3,7 @@ import '@testing-library/cypress/add-commands';
 Cypress.Commands.add('login', () => {
   cy.setCookie('access', Cypress.env('access'));
 });
+
+Cypress.Commands.add('logout', (statusCode: number) => {
+  cy.intercept('POST', 'http://localhost:8080/auth/logout', { statusCode }).as('logout');
+});
