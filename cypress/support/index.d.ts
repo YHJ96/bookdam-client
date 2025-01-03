b986c580acc22ddf45e9e78f7ebb5ad6467ecf6f
YHJ96 <yhj878812@naver.com>
2025-01-03 23:58:18 +0900

    test: trash bookmark page test

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -3,5 +3,10 @@ declare namespace Cypress {
   interface Chainable {
     login(): Chainable<void>;
     logout(statusCode: number): Chainable<void>;
+    getTags(statusCode: number, body: any): Chainable<void>;
+    getBookmarks(statusCode: number, body: any): Chainable<void>;
+    getTrashBookmarks(statusCode: number, body: any): Chainable<void>;
+    redoTrashBookmark(statusCode: number, id: number): Chainable<void>;
+    undoTrashBookmark(statusCode: number, id: number): Chainable<void>;
   }
 }
