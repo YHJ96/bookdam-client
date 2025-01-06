3eb93159cd25f2fbebe0729d275e8ede7d59d4f6
YHJ96 <yhj878812@naver.com>
2025-01-07 00:46:50 +0900

    test: floating button component test

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -3,8 +3,10 @@ declare namespace Cypress {
   interface Chainable {
     login(): Chainable<void>;
     logout(statusCode: number): Chainable<void>;
+    createOgTag(statusCode: number, body: any): Chainable<void>;
     getTags(statusCode: number, body: any): Chainable<void>;
     getBookmarks(statusCode: number, body: any): Chainable<void>;
+    createBookmark(statusCode: number, body: any): Chainable<void>;
     updateBookmark(statusCode: number, id: number, body: any): Chainable<void>;
     removeBookmark(statusCode: number, id: number): Chainable<void>;
     getTrashBookmarks(statusCode: number, body: any): Chainable<void>;
