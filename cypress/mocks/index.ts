ff54feca6cef203877177e3179751827c8244b80
YHJ96 <yhj878812@naver.com>
2025-01-06 20:28:41 +0900

    test: bookmark search input component test

--- cypress/mocks/index.ts
+++ cypress/mocks/index.ts
@@ -1,11 +1,13 @@
-const BOOKMARKS_DATA = [
+export const TAGS_DATA = ['IT', 'ETC'];
+
+export const BOOKMARKS_DATA = [
   {
     title: '네이버',
     description: '네이버 메인에서 다양한 정보와 유용한 컨텐츠를 만나 보세요',
     url: 'https://naver.com',
     image: 'https://s.pstatic.net/static/www/mobile/edit/2016/0705/mobile_212852414260.png',
     id: 142216,
-    tags: [],
+    tags: ['IT'],
     createdAt: '2025-01-03T08:54:14.136Z',
   },
   {
@@ -15,12 +17,12 @@ const BOOKMARKS_DATA = [
     image:
       'https://zyhedgwubqhgbbifgwmd.supabase.co/storage/v1/object/sign/image/empty.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZS9lbXB0eS53ZWJwIiwiaWF0IjoxNzMzMjExNTI5LCJleHAiOjE3NjQ3NDc1Mjl9.2aBu-Xc8hUQN5ZWFOeFxCIiDd-ESxSH524Bcjc1M3DU&t=2024-12-03T07%3A38%3A49.188Z',
     id: 612970,
-    tags: [],
+    tags: ['ETC'],
     createdAt: '2025-01-03T08:54:22.841Z',
   },
 ];
 
-const TRASH_BOOKMARKS_DATA = [
+export const TRASH_BOOKMARKS_DATA = [
   {
     title: '네이버',
     description: '네이버 메인에서 다양한 정보와 유용한 컨텐츠를 만나 보세요',
@@ -42,18 +44,23 @@ const TRASH_BOOKMARKS_DATA = [
   },
 ];
 
-const BOOKMARKS_DATA_LOCAL_STORAGE = {
+export const BOOKMARKS_DATA_LOCAL_STORAGE = {
   state: {
     bookmarks: BOOKMARKS_DATA,
   },
   version: 0,
 };
 
-const TRASH_BOOKMARKS_DATA_LOCAL_STORAGE = {
+export const TRASH_BOOKMARKS_DATA_LOCAL_STORAGE = {
   state: {
     bookmarks: TRASH_BOOKMARKS_DATA,
   },
   version: 0,
 };
 
-export { BOOKMARKS_DATA_LOCAL_STORAGE, BOOKMARKS_DATA, TRASH_BOOKMARKS_DATA_LOCAL_STORAGE };
+export const TAGS_DATA_LOCAL_STORAGE = {
+  state: {
+    tags: TAGS_DATA,
+  },
+  version: 0,
+};
