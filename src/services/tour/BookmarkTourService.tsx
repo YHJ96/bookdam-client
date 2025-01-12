1f7f15d8bd7acc851f48ad777b7da351c3fa5e66
YHJ96 <yhj878812@naver.com>
2025-01-13 00:12:48 +0900

    feat: tour simplify

--- src/services/tour/BookmarkTourService.tsx
+++ src/services/tour/BookmarkTourService.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React, { useMemo, useRef } from 'react';
-import type { CallBackProps, Step, StoreHelpers } from 'react-joyride';
+import React from 'react';
+import type { CallBackProps, Step } from 'react-joyride';
 
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
@@ -14,8 +14,6 @@ const Joyride = dynamic(() => import('react-joyride'), { ssr: false });
 
 function BookmarkTourService() {
   const { isTour } = useTourStore();
-  const helperRef = useRef<StoreHelpers>();
-  const timerRef = useRef<ReturnType<typeof setTimeout>>();
   const router = useRouter();
 
   const steps: Step[] = [
@@ -51,22 +49,8 @@ function BookmarkTourService() {
     },
     {
       target: '#bookmark-options-trigger',
-      content: '옵션을 클릭해보세요.',
+      content: '북마크 옵션으로 수정과 삭제를 할 수 있습니다.',
       placement: 'bottom',
-      disableBeacon: true,
-      hideFooter: true,
-      spotlightClicks: true,
-    },
-    {
-      target: '#bookmark-options-content',
-      content: '수정 버튼을 눌러 북마크를 수정합니다.',
-      placement: 'right',
-      disableBeacon: true,
-    },
-    {
-      target: '#bookmark-options-content',
-      content: '삭제 버튼을 눌러 북마크를 삭제합니다.',
-      placement: 'right',
       disableBeacon: true,
     },
     {
@@ -74,84 +58,19 @@ function BookmarkTourService() {
       content: '엑셀 다운로드 버튼을 눌러 북마크를 다운로드합니다.',
       placement: 'right',
       disableBeacon: true,
-      spotlightClicks: true,
     },
     {
       target: '#floating-button',
-      content: '버튼을 클릭해 보세요.',
+      content: '버튼을 눌러 북마크를 추가할 수 있습니다.',
       placement: 'left',
       disableBeacon: true,
-      hideFooter: true,
-      spotlightClicks: true,
-    },
-    {
-      target: '#bookmark-create-dialog',
-      content: 'URL은 필수로 입력하고 제목과 내용은 선택적으로 입력할 수 있습니다.',
-      placement: 'left',
-      disableBeacon: true,
-      spotlightClicks: false,
     },
   ];
 
-  const domEvent = useMemo(() => {
-    const handleDismissEvent = () => {
-      timerRef.current = setTimeout(() => helperRef.current?.next(), 300);
-      document.addEventListener('pointerdown', domEvent.preventDismiss, true);
-      document.addEventListener('focusin', domEvent.preventDismiss, true);
-    };
-
-    const preventDismiss = (e: Event) => {
-      e.stopPropagation();
-      e.preventDefault();
-    };
-
-    return { handleDismissEvent, preventDismiss };
-  }, []);
-
   const handleJoyrideCallback = (data: CallBackProps) => {
-    const { index, type, status } = data;
-    const $button = document.querySelector<HTMLButtonElement>('button[aria-haspopup="menu"]');
-    const $floatingButton = document.querySelector<HTMLButtonElement>('#floating-button');
-
-    if (index === 5 && type === 'tooltip') {
-      $button?.addEventListener('pointerdown', domEvent.handleDismissEvent);
-      document.addEventListener('click', domEvent.preventDismiss);
-    }
-
-    if (index === 8 && type === 'tooltip') {
-      document.removeEventListener('pointerdown', domEvent.preventDismiss, true);
-      document.removeEventListener('focusin', domEvent.preventDismiss, true);
-      document.removeEventListener('click', domEvent.preventDismiss);
-      $button?.removeEventListener('pointerdown', domEvent.handleDismissEvent);
-      clearTimeout(timerRef.current);
-
-      queueMicrotask(() => {
-        const pointerEvent = new PointerEvent('pointerdown');
-        document.dispatchEvent(pointerEvent);
-      });
-    }
-
-    if (index === 9 && type === 'tooltip') {
-      $floatingButton?.addEventListener('click', domEvent.handleDismissEvent);
-    }
-
-    if (index === 10 && type === 'tooltip') {
-      document.body.style.pointerEvents = 'auto';
-    }
-
-    if (status === 'finished') {
-      document.removeEventListener('pointerdown', domEvent.preventDismiss, true);
-      document.removeEventListener('focusin', domEvent.preventDismiss, true);
-      $floatingButton?.removeEventListener('click', domEvent.handleDismissEvent);
-      clearTimeout(timerRef.current);
-
-      queueMicrotask(() => {
-        const pointerEvent = new PointerEvent('pointerdown');
-        document.dispatchEvent(pointerEvent);
-      });
-
-      router.push('/trash');
-    }
+    const { status } = data;
+    if (status !== 'finished') return;
+    router.push('/trash');
   };
 
   return (
@@ -160,7 +79,6 @@ function BookmarkTourService() {
       continuous={true}
       steps={steps}
       callback={handleJoyrideCallback}
-      getHelpers={(helper) => (helperRef.current = helper)}
       disableOverlayClose={true}
       hideCloseButton={true}
       tooltipComponent={(context) => <TourTooltip {...context} />}
